2d3a624b2bb7791cec0697d54cfead2e29446cd1
XuemeiGu <njuxmgu@gmail.com>
2024-02-28 17:16:15 +0100

    clean code

--- create_concepts/Domain_Concept/s5_improve_manually_concepts.py
+++ create_concepts/Domain_Concept/s5_improve_manually_concepts.py
@@ -3,8 +3,8 @@ from datetime import datetime, date
 
 ## repeated initial_num=0,1,2,...., start from 0, the second run will be 1, etc.
 # the new_file_name is the final filtered concepts  
-initial_num=16
-file_name='full_concepts_for_openalex_'+str(initial_num)+'.txt'
+initial_num=0
+file_name='full_domain_concepts_'+str(initial_num)+'.txt'  #'full_concepts_for_openalex_'+str(initial_num)+'.txt'
 curr_file = os.path.join("full_concepts",file_name) 
 new_concept_list=[]
 
@@ -23,7 +23,7 @@ formatted_time = now_time.strftime("%d-%m-%Y %H:%M:%S")
 print(f"{formatted_time}, Concepts: {concept_count} ; Remove: {idx-concept_count+1} ")
 
 new_num=initial_num+1
-new_file_name='full_concepts_for_openalex_'+str(new_num)+'.txt'
+new_file_name='full_domain_concepts_'+str(new_num)+'.txt' # the final concept file will be renamed as full_domain_concepts.txt
 ### 
 with open(new_file_name, 'w') as file:
     for item in new_concept_list:
